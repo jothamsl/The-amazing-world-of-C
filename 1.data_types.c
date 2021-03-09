f36d2eb86daaadb4e264aab1104eabd2b508fab9
Jothamsl <jothamsl1@gmail.com>
2021-03-09 20:13:26 +0100

    Covered basic C data types

---
+++ 1.data_types.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    // Basic C data types
+    // The "unsigned" keyword pushes value range from 0 to max
+
+    // Int
+    int a = 21;            // Integer values from −32,768 to 32,767
+    signed int b = 249;    // Signed Integer values from −32,768 to 32,767
+    unsigned int c = 4902; // Unsigned Integer values from 0 to 65,535
+    
+    // Char
+    char d = 95;           // Char values from −128 to 127
+    signed char e = 'f';   // Signed Char values from -128 to 127
+    unsigned char f = 'g'; // Unsigned Char values from 0 to 255
+
+    // Float (There are no signed and unsigned forms of float and double)
+    float g = 4.3;         // Float  
+    double c = 4.325;      // Double 
+
+}
