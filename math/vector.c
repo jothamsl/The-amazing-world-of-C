5a2afbe4894253b31d8d59c0336368487f84da63
Jothamsl <jothamsl1@gmail.com>
2021-05-15 17:56:27 +0100

    Refined vector implementation

--- math/vector.c
+++ math/vector.c
@@ -1,6 +1,5 @@
 #include <stdio.h> 
 #include <stdlib.h>
-#include <math.h> 
 
 #define MAX_VEC_CAPACITY 400
 
@@ -13,6 +12,7 @@ typedef struct vector {
 /* 
  * Initialize_vector
  *
+ * Allocates memory for vector
  * @param: void *vals -> Input data for vector
  */
 
@@ -28,13 +28,25 @@ vector initialize_vect(int *vals, int s)
 }
 
 /* 
- * Sum_vectors
+ * Destroy Vector
  *
- * @param: vector vec1 & vector vec2 -> Input vectors with
- * same size
+ * Destroys a vector by deallocating the memory of it
+ * @param: vector *v -> Pointer to vector
  */
 
-vector vect_sum(vector vec1, vector vec2)
+void destroy_vect(vector *v)
+{
+    free(v);
+}
+
+/* 
+ * Sum vectors
+ *
+ * Returns Array of summed values
+ * @param: vector vec1 & vector vec2 -> Input vectors with same size
+ */
+
+int *vect_sum(vector vec1, vector vec2)
 {
     if (vec1.size != vec2.size)
     {  
@@ -43,29 +55,52 @@ vector vect_sum(vector vec1, vector vec2)
     }
 
     int size = vec1.size;
-    int res[size]; 
+    int *res = (int *) malloc(size * size); 
     for (int i = 0; i < size; i++)
     {
-        res[i] = vec1.data[i] + vec2.data[i];
+        res[i] = (int)vec1.data[i] + (int)vec2.data[i];
     }
-    vector f = initialize_vect(res, size);
-    return f;
+    return res;  
+}
+
+/* 
+ * Difference of vectors
+ *
+ * Returns Array of minused values
+ * @param: vector vec1 & vector vec2 -> Input vectors with same size
+ */
+
+int *vect_minus(vector vec1, vector vec2)
+{
+    if (vec1.size != vec2.size)
+    {  
+        printf("\033[0;31mCannot minux vec1 of size %zu and vec2 of size %zu\n\033[0m", vec1.size, vec2.size);
+        exit(1);
+    }
+
+    int size = vec1.size;
+    int *res = (int *) malloc(size * size); 
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = (int)vec1.data[i] - (int)vec2.data[i];
+    }
+    return res;  
 }
 
 /* 
  * Display vector 
  *
- * @param: vector v -> vector to display 
+ * Displays vector representation in array format
+ * @param: vector v -> Pointer of vector to display 
  */
 
-void display_vec(vector v)
+void display_vec(vector *v)
 {
     printf("[");
-    for (int i = 0; i < v.size; i++)
-    {
-        if (i == v.size-1)
-            printf("%d]", v.data[i]), exit(0);
-       printf("%d,", v.data[i]); 
+    for (int i = 0; i < v->size; i++) {
+        if (i == v->size-1)
+            printf("%d]\n", v->data[i]), exit(0);
+       printf("%d, ", v->data[i]); 
     }
 }
 
@@ -75,6 +110,7 @@ int main(void)
     int val2[] = {1, 5, 0, 9}, size2 = sizeof(val2)/sizeof(int);
     vector veck1 = initialize_vect(val1, size1);
     vector veck2 = initialize_vect(val2, size2);
-    vector sum = vect_sum(veck1, veck2);
-    display_vec(sum);
+    vector sum = initialize_vect(vect_minus(veck1, veck2), veck1.size);
+    display_vec(&sum);
+    destroy_vect(&sum);
 }
