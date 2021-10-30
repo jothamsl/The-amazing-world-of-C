2ac338862593724d3a4865fae74e1a04657ee2bf
Jotham Soboyejo <55670042+jothamsl@users.noreply.github.com>
2021-10-30 07:44:03 +0100

    Done first 2 problems in ANSI C programming book

---
+++ ANSI C Programming Language Exercises/1-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("hello");
+  printf("world");
+  printf("\a"); // Makes and alert beep
+  printf("\e"); // escape character
+}
---
+++ ANSI C Programming Language Exercises/1-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;   // lower limit of temperature scale
+  upper = 300; // upper limit
+  step = 20;   // step size
+}
